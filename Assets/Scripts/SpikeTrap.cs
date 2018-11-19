f7dc14a0253ea9d0c6fcb7143b658dd0651f54ea
DejF <gi3r4s@gmail.com>
2018-11-19 06:25:17 +0100

    Traps' prefabs are ready

---
+++ Assets/Scripts/SpikeTrap.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour {
+
+    int durubality = 3;
+    float cooldown = 5;
+    bool isAvailable = false;
+    float startTime;
+    float endTime;
+
+    List<GameObject> monstersInRange;
+    Animation animation;
+    Collider collider;
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<Collider>();
+        animation = GetComponent<Animation>();
+        
+        monstersInRange = new List<GameObject>();
+        animation.wrapMode = WrapMode.Once;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (durubality == 0 && !animation.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+        endTime = Time.time;
+        float currentDiffrence = endTime - startTime;
+
+        if(currentDiffrence >= cooldown || isAvailable)
+        {
+            FindMonstersInRnge();
+            if(!animation.isPlaying && monstersInRange.Count != 0)
+            {
+                animation.Play();
+                foreach(GameObject monster in monstersInRange)
+                {
+                    monster.SendMessage("DealDamage", new DamageParameters { damageAmount = 1000, duration = 1.5f, slowDownFactor = 0.3f });
+                }
+                durubality--;
+                isAvailable = false;
+                startTime = Time.time;
+            }
+        }
+
+    }
+
+    void FindMonstersInRnge()
+    {
+        monstersInRange.Clear();
+
+        float minX = collider.bounds.min.x;
+        float maxX = collider.bounds.max.x;
+        float minZ = collider.bounds.min.z;
+        float maxZ = collider.bounds.max.z;
+
+        GameObject[] allMonsters = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (GameObject monster in allMonsters)
+        {
+            if (monster.transform.position.x > (minX) && monster.transform.position.x < (maxX) && monster.transform.position.z > (minZ) && monster.transform.position.z < (maxZ))
+                monstersInRange.Add(monster);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TarSplash.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TarSplash : MonoBehaviour
+{
+    private List<GameObject> monsters;
+    private int numberOfTargets = 5;
+
+    // Use this for initialization
+    private void Start()
+    {
+        monsters = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        AddEnemies();
+        CheckAndSlow();
+        CleanUp();
+    }
+
+    private void AddEnemies()
+    {
+        GameObject[] allMonsters = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (GameObject monster in allMonsters)
+        {
+            if (Vector3.Distance(monster.transform.position, transform.position) <= transform.localScale.x / 2)
+            {
+                if (!monsters.Contains(monster) && monsters.Count < numberOfTargets)
+                {
+                    monsters.Add(monster);
+                }
+            }
+        }
+    }
+
+    private int CheckAndSlow()
+    {
+        int numberOfSlowed = 0;
+        for (int i = 0; i < monsters.Count; i++)
+        {
+            if (Vector3.Distance(monsters[i].transform.position, transform.position) <= transform.localScale.x / 2)
+            {
+                monsters[i].SendMessage("DealDamage", new DamageParameters { damageAmount = 0, duration = 0.1f, slowDownFactor = 0.3f });
+                numberOfSlowed++;
+            }
+        }
+
+        return numberOfSlowed;
+    }
+
+    private void CleanUp()
+    {
+        for (int i = 0; i < monsters.Count; i++)
+        {
+
+            if (monsters.Count == numberOfTargets && CheckAndSlow() == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
